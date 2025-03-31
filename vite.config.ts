6b1dee03db9ce3c8b4b685e507abc5a5ee8c94c0
Artyom Ulanchik <artyom.u@outlook.com>
2025-04-01 00:31:50 +0100

    setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,6 +7,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
     },
   },
 });
