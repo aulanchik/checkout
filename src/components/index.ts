75f6c706c625825adf1fba7d7d52b3225444ac57
Artyom Ulanchik <artyom.u@outlook.com>
2025-04-02 12:22:36 +0100

    added Basket

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import Basket from './Basket';
+import Summary from "./Summary";
+import PriceInput from "./PriceInput";
+import PricingRules from './PricingRules';
+
+export {
+    Basket,
+    Summary,
+    PriceInput,
+    PricingRules
+}
