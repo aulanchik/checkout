723f1b566a577b1245a1d9be3ed2f4a9c722ba6b
Artyom Ulanchik <artyom.u@outlook.com>
2025-04-02 12:24:03 +0100

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 import { FC, JSX } from "react";
+import { BasketProvider } from "./contexts/BasketContext";
+import { PricingRules, Basket } from '@/components'
 
-const App: FC = (): JSX.Element => <>Hello World</>;
+const App: FC = (): JSX.Element => (
+    <BasketProvider>
+        <div>
+            <header>
+                <h1>Store Checkout</h1>
+            </header>
+
+            <main>
+                <div>
+                    <div>
+                        <section>
+                            <PricingRules />
+                        </section>
+
+                        <section>
+                            <Basket />
+                        </section>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </BasketProvider>
+)
 
 export default App;
